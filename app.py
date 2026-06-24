8bbeed09b3387b506a2dd02ba20ebf49050fb675
André Miguel Silveira <the717q@gmail.com>
2026-06-24 22:32:42 +0100

    feitas alterações significativas no dashboard, usado o pandas para a visualização dos dados

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
+# streamlit run app.py
 st.set_page_config(page_title='Dashboard', layout='wide')
 
 st.title('Amazon Dashboard')
@@ -9,24 +11,10 @@ st.markdown('Dashboard de produtos')
 @st.cache_data 
 def load_data():
     file_name = "clean_output.parquet"
-    df = pd.read_csv(file_name, parse_dates=["Order Date"])
+    df = pd.read_parquet(file_name)
     return df
 
 df = load_data()
-
-st.sidebar.header("Filtros")
-
-sorted_categories = sorted(df["Category"].unique())
-categories = st.sidebar.multiselect(
-    "Categoria",
-    options=sorted_categories,
-    default=sorted_categories
-)
-
-years = sorted(df["rating"].unique())
-year_range = st.sidebar.slider(
-    "Rating",
-)
 
 # --------------------------------------------------
 # Parte superior com KPIs
@@ -36,6 +24,65 @@ total_rating =  df["rating_count"].sum()
 total_category =  df["category"].sum()
 
 # Vamos dividir a área em 3 colunas para mostrar os KPIs lado a lado
-col1, col2, col3 = st.columns(3)
-col1.metric("💰 Vendas Totais", f"${total_rating:,.0f}")
-col2.metric("📈 Lucro Total", f"${total_category:,.0f}")
+#col1, col2, col3 = st.columns(3)
+#col1.metric("💰 Nº de ratings", f"{total_rating}")
+#col2.metric("📈 Lucro Total", f"${total_category:,.0f}")
+
+# Distribuição de preços após desconto:
+st.divider()
+
+
+# Create price bins (like grouping)
+st.subheader("Distribuição de Preços após desconto")
+
+price_dist = (
+    df
+    .assign(price_bin=pd.cut(df['discounted_price'], bins=20).astype(str))
+    .groupby('price_bin')
+    .size()
+)
+
+st.bar_chart(price_dist)
+
+# Plot
+st.bar_chart(price_dist)
+
+
+st.divider()
+
+#####
+
+st.subheader("Distribuição da percentagem de desconto")
+
+dist = (
+    df['discount_percentage']
+    .value_counts(bins=20)
+    .sort_index()
+)
+
+st.bar_chart(dist)
+
+
+st.divider()
+
+st.subheader("Distribuição da variável rating")
+
+dist = (
+    df['rating']
+    .value_counts(bins=20)
+    .sort_index()
+)
+
+st.bar_chart(dist)
+
+####
+st.subheader('Top Produtos com Mais Avaliações')
+
+top_rated = (
+    df
+    .nlargest(10, 'rating_count')
+    .assign(short_name=lambda x: x['product_name'].str[:40] + "...")
+    .set_index('short_name')['rating_count']
+)
+
+st.bar_chart(top_rated)
